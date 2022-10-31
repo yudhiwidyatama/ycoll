3f687341e25931f96ad61a802d1ba17e83eb7326
yudhiwidyatama <yudhi.widyatama@gmail.com>
2022-10-31 12:05:28 +0700

    update untuk overlay docker storage

--- lvmusagecoll.py
+++ lvmusagecoll.py
@@ -1,6 +1,7 @@
 import time
 import random
 from os import path
+from pathlib import Path
 import yaml
 import socket
 import os
@@ -8,8 +9,10 @@ from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFam
 from prometheus_client import start_http_server
 a = "docker inspect --format \"{{.GraphDriver.Data.DeviceName}}\""
 b = "docker ps --format \"{{.ID}} {{.Names}}\""
-c = "docker ps --format \"{{.ID}}\" | xargs docker inspect -f \"{{.ID}} {{.Name}} {{.GraphDriver.Data.DeviceName}}\""
+c = "docker ps --format \"{{.ID}}\" | xargs docker inspect -f \"{{.ID}}?{{.Name}}?{{.GraphDriver.Data.DeviceName}}?{{.GraphDriver.Data.WorkDir}}\""
 totalRandomNumber = 0
+def get_size(folder: str) -> int:
+    return sum(p.stat().st_size for p in Path(folder).rglob('*'))
 class YCollector(object):
     def __init__(self):
         pass
@@ -20,11 +23,12 @@ class YCollector(object):
         with os.popen(c) as f:
           l1 = f.readlines()
           for litem in l1:
-            arr = litem.strip().split(" ")
+            arr = litem.strip().split("?")
             container_name = arr[1]
             container_id = arr[0]
             devicename = arr[2]
-            if True:
+            workdirpath = arr[3]
+            if devicename != "<no value>":
                 devsplit = devicename.split("-")
                 lvmid = devsplit[3].strip()
                 lvmmnt = "/var/lib/docker/devicemapper/mnt/" + lvmid
@@ -45,6 +49,23 @@ class YCollector(object):
                 else:
                   lvmused.add_metric([myhostname,container_name],stat2['usedbytes'])
                   lvmfree.add_metric([myhostname,container_name],stat2['freebytes'])
+            else:
+                stat1 = os.statvfs(workdirpath)
+                stat2 = {}
+                stat2['hostname'] = myhostname
+                stat2['container_name'] = container_name
+                stat2['freebytes'] = stat1.f_bfree * stat1.f_bsize
+                stat2['usedbytes'] = get_size(workdirpath)
+                k8sname = container_name.split("_")
+                if len(k8sname)>=4 :
+                   namespacevalue = k8sname[3]
+                   podname = k8sname[2]
+                   container_name = k8sname[1]
+                   lvmused.add_metric([myhostname,container_name,namespacevalue,podname],stat2['usedbytes'])
+                   lvmfree.add_metric([myhostname,container_name,namespacevalue,podname],stat2['freebytes'])
+                else:
+                   lvmused.add_metric([myhostname,container_name],stat2['usedbytes'])
+                   lvmfree.add_metric([myhostname,container_name],stat2['freebytes'])
         yield lvmused
         yield lvmfree
 if __name__ == "__main__":
